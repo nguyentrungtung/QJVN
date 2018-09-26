db1b50ece631b873c172a2816223f657afdea6f9
Tung Nguyen Trung <tung.nguyen.intern@ntq-solution.com.vn>
2018-09-26 20:11:16 +0700

    firt-commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    $().click(function(e) {
+
+    });
+
+});
+
+function openSearch() {
+    document.getElementById("myOverlay ").style.display = "block ";
+}
+
+function closeSearch() {
+    document.getElementById("myOverlay ").style.display = "none ";
+}
+// .siblings().removeClass('active')
